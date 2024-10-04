ae6f600bbf765356c9c25f2361d2c065f4fb24a3
Michael Ouazzani <morphois77@web.de>
2024-10-04 16:56:40 +0000

    feat: erstes Argument und Zahl Abfrage

--- element.sh
+++ element.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "Please provide an element as an argument."
+if [ "$#" -eq 0 ]; then
+  echo "Please provide an element as an argument."
+  else
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align --tuples-only -c"
+ELEMENT_NUMBER_1=$1
+
+SQL_QUERY="SELECT * FROM properties JOIN elements USING(atomic_number) WHERE atomic_number=$ELEMENT_NUMBER_1"
+
+
+ATOMIC_NUMBER=$($PSQL "$SQL_QUERY" 2>&1)
+if [[ -z $ATOMIC_NUMBER || $ATOMIC_NUMBER == *"ERROR:"* ]]
+then
+  echo "Element with atomic number $ELEMENT_NUMBER_1 not found."
+    exit 1
+else
+
+echo -e "\n$ATOMIC_NUMBER"
+fi
+
+fi
